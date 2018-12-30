8b97549f1f367fc4e556cb61a05ba5370a2b865a
miroma21 <miromax21@gmail.com>
2018-12-30 10:40:46 +0400

    Initial Commit

---
+++ l-itinerary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  l-itinerary
+//
+//  Created by Princess Max on 30.12.2018.
+//  Copyright © 2018 Princess Max. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
